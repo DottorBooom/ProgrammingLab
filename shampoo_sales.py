581a767a5122999d7e11ec9fb719b91ecb1e3645
DottorBooom <94912692+DottorBooom@users.noreply.github.com>
2021-11-26 21:11:29 +0100

    Crazione di una classe per la gestione del file
    Creata una classe nominata "CSVFile" che prende come parametro il percorso del file, il quale verrà attribuito al parametro "name" della classe. I due metodi per la somma dei prezzi degli shampi sono stati spostati all' interno della classe ed è stato creato un metodo "get_data()" che prende come parametro un file e restituisce la stampa di tutti i suoi valori sotto forma di lista. In oltre il metodo "__init__" è stato modificato per permettere l'inserimento del percorso del file.

--- shampoo_sales.py
+++ shampoo_sales.py
@@ -1,22 +1,37 @@
 #Dato il file shampoo_sales.txt o .csv, sommare tutti i valori degli shampi e stamparli a video.
 
+from os import name
 import sys
 
-#Versione semplice senza l'utilizzo della funzione sum
-def somma_v1(my_file): 
-    som = 0
-    for i in my_file:
-        som += float((i.split(",")[1]))
-    return som
+class CSVFile:
 
-#Versione one-line code con utilizzo della funzione sum
-def somma_v2(my_file):
-    return sum((float(i.split(",")[1]) for i in my_file))
+    def __init__(self, name):
+        self.name = name
 
-def main(): 
-    with open(r'C:\Users\Davide\Documents\Università\ProgrammazioneLab\Esercizi\Shampoo_sales.txt', 'r') as f:
-        print(somma_v1(f)) #Cambiare 1 o 2 dopo v per testare le due funzioni
-        f.close()
+    #Versione semplice senza l'utilizzo della funzione sum
+    def somma_v1(my_file): 
+        som = 0
+        for i in my_file:
+            som += float((i.split(",")[1]))
+        return som
 
-if __name__ == '__main__':
-    sys.exit(main())
+    #Versione one-line code con utilizzo della funzione sum
+    def somma_v2(my_file):
+        return sum((float(i.split(",")[1]) for i in my_file))
+
+
+    def get_data(my_file):
+        j = []
+        for i in my_file:
+            j.append(i.split(","))
+        return j
+        
+def main():
+    name = r"C:\Users\Davide\Documents\Università\ProgrammazioneLab\Esercizi\Shampoo_sales.csv"
+    with open(name, 'r') as f: #Ricordarsi di modificare il percorso del proprio file
+        c = CSVFile(name)
+        print(CSVFile.get_data(f))
+        print(c.name)
+    f.close()
+
+main()
