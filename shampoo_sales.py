e8e468e1064682af8440a8826a6f21223d00df61
DottorBooom <94912692+DottorBooom@users.noreply.github.com>
2021-11-29 17:51:51 +0100

    Creazione di una sotto classe e gestione errori
    Implementata la gestione dell'errore per file non trovato in base al percorso passato alla classe CSVFile. Creazione di una sotto classe NumericalCSV per convertire solo la seconda colonna a float. Creazione di un secondo file .cvs contenente due righe e la gestione degli errori provocati da quest'ultime due.

--- shampoo_sales.py
+++ shampoo_sales.py
@@ -1,37 +1,57 @@
 #Dato il file shampoo_sales.txt o .csv, sommare tutti i valori degli shampi e stamparli a video.
 
-from os import name
+from os import close, name
 import sys
 
 class CSVFile:
 
     def __init__(self, name):
         self.name = name
+        try:
+            self.file = open(name, 'r')
+        except:
+            print('Il file non è stato trovato')
 
     #Versione semplice senza l'utilizzo della funzione sum
-    def somma_v1(my_file): 
+    def __somma_v1__(self): 
         som = 0
-        for i in my_file:
+        for i in self.file:
             som += float((i.split(",")[1]))
         return som
 
     #Versione one-line code con utilizzo della funzione sum
-    def somma_v2(my_file):
-        return sum((float(i.split(",")[1]) for i in my_file))
+    def __somma_v2__(self):
+        return sum((float(i.split(",")[1]) for i in self.file))
 
-
-    def get_data(my_file):
+    def __get_data__(self):
         j = []
-        for i in my_file:
+        for i in self.file:
             j.append(i.split(","))
+        return j    
+
+class NumericalCSVFile(CSVFile):
+
+    def __init__(self, name):
+        super().__init__(name)
+    
+    def __strToFloat__(self):
+        j = []
+        for i in self.file:
+            try:
+                j.append([i.split(",")[0],float(i.split(",")[1])])
+            except ValueError:
+                print('Non posso convertire "i[0]" a valore numerico!')
+                print('Ho avuto un errore di VALORE. "i[0]" valeva "{}".'.format(i.split(",")[1]))
+            except TypeError:
+                print('Non posso convertire "i[0]" a valore numerico!')
+                print('Ho avuto un errore di TIPO. "i[0]" era di tipo "{}".'.format(type(i.split(",")[1])))
         return j
         
 def main():
-    name = r"C:\Users\Davide\Documents\Università\ProgrammazioneLab\Esercizi\Shampoo_sales.csv"
-    with open(name, 'r') as f: #Ricordarsi di modificare il percorso del proprio file
-        c = CSVFile(name)
-        print(CSVFile.get_data(f))
-        print(c.name)
-    f.close()
+    name1 = r"C:\Users\Davide\Documents\Università\ProgrammazioneLab\Esercizi\Shampoo_sales.csv"
+    name2 = r"C:\Users\Davide\Documents\Università\ProgrammazioneLab\Esercizi\Shampoo_sales_errors.csv"
+    #Ricordarsi di modificare il percorso del proprio file
+    c = NumericalCSVFile(name2)
+    print(c.__strToFloat__())
 
-main()
+main()  
